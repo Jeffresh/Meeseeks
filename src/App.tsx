1519f362bbebbcfaccdea86392b4207694dc0ea6
Jeff <jeff.uca@gmail.com>
2020-02-26 16:10:48 +0100

    [ADD] Base

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>Meeseeks says: Hello world!</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
