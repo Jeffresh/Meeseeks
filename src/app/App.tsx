3584a132f89dc957c88bfa2c1c7ba5ca74429e86
Jeff <jeff.uca@gmail.com>
2020-03-01 22:05:51 +0100

    chore: add components navbar and footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { Footer } from '~Components/Footer'
 import { Hello } from '~Components/Hello'
+import { Navbar } from '~Components/Navbar'
 
 export const App = (): JSX.Element => {
-  return <Hello />
+  return (
+    <>
+      <Navbar />
+      <Hello />
+      <Footer />
+    </>
+  )
 }
