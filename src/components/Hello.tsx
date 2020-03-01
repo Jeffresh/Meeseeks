68ddfd14f9b4ffd08a4b4c15baaaa5ea7d634057
Jeff <jeff.uca@gmail.com>
2020-03-01 22:05:01 +0100

    chore: mod hello component now body behavior/rol

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,3 +1,38 @@
+import { Container, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
 
-export const Hello = (): JSX.Element => <div>Hello</div>
+const useStyles = makeStyles(theme => ({
+  body: {
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+}))
+
+export const Hello = (): JSX.Element => {
+  const classes = useStyles()
+
+  return (
+    <Container maxWidth="sm" component="main" className={classes.body}>
+      <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        color="textPrimary"
+        gutterBottom
+      >
+        Hello
+      </Typography>
+
+      <Typography
+        variant="h5"
+        align="center"
+        color="textSecondary"
+        component="p"
+      >
+        The best WebPage you even seen in your life, and in your death...
+      </Typography>
+    </Container>
+  )
+}
