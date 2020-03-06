c60a92c309a118c82a693c4927d24bdbbfe17dc7
Jeff <jeff.uca@gmail.com>
2020-03-06 09:18:23 +0100

    chore: add component to list characters

--- src/pages/Character.tsx
+++ src/pages/Character.tsx
@@ -1,4 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AxiosResponse } from 'axios'
 import { RFC } from '~Types/react'
+import * as api from '../services/api'
 
-export const Character: RFC = () => <div>Characters</div>
+export const Character: RFC = () => {
+  const [CharacterList, SetCharacterList] = useState()
+  const listCharacters = (characters: api.Character[]): JSX.Element => {
+    return (
+      <div>
+        {characters.map((character: api.Character) => (
+          <div key={character.id}>
+            <p>Id: {character.id}</p>
+            <p>Name: {character.name}</p>
+            <p>Episodes:{character.episode}</p>
+          </div>
+        ))}
+      </div>
+    )
+  }
+  const characters = async (): Promise<AxiosResponse<any>> => api.getCharacter()
+  useState(
+    characters()
+      .then(r =>
+        SetCharacterList(listCharacters(r.data.results).props.children),
+      )
+      .catch(),
+  )
+
+  return <>{CharacterList}</>
+}
